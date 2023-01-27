514fdcfbbd9d2788522603a2ff715af7e4eeb7cc
christhed <103331504+christhed@users.noreply.github.com>
2023-01-27 15:13:11 -0500

    Update de fin de journée
    Production d'histogrammes automatisée

--- README.md
+++
@@ -1,2 +0,0 @@
-# projet_velo_portfolio
- Projet portfolio qui extrait des données de la Ville de Montréal sur le passage de vélos aux stations de comptage. Cartographie et création de figures automatique à partir des données extraites
--- visualisation.py
+++ visualisation.py
@@ -5,13 +5,12 @@ toutes_annees = pd.read_csv('data/toutes_annees.csv', sep=',')
 
 #Création d'un histogramme pour chaque station de comptage
 for station in toutes_annees['Nom']:
-    plt.figure(figsize=(35,25))
+    plt.figure(figsize=(35,30))
     plt.style.use('ggplot')
     plt.bar(toutes_annees.columns[1:-3], toutes_annees.loc[toutes_annees['Nom'] == station].iloc[0,1:-3])
-    plt.title('Station de comptage ' + station, fontsize=30)
-    plt.xticks(rotation=45, fontsize=15)
-    plt.ylabel('Nombre de vélos', fontsize=20)
+    plt.title('Station de comptage ' + station, fontsize=50)
+    plt.xticks(rotation=35, fontsize=25)
+    plt.ylabel('Nombre de vélos', fontsize=35)
     plt.yticks(fontsize=15)
     plt.savefig('graphs/' + station + '.png')
     plt.close()
-
